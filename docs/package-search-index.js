14166945f49d72b5ac9663ed7528f0c93804031e
Siroshun09 <siro.shun8@gmail.com>
2020-02-15 23:13:23 +0900

    [Clean] docs/ 初期化

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"com.github.siroshun09.spawners"},{"l":"com.github.siroshun09.spawners.events"},{"l":"com.github.siroshun09.spawners.listeners"},{"l":"com.github.siroshun09.spawners.stack"},{"l":"すべてのパッケージ","url":"allpackages-index.html"}]
