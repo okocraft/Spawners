b802dd31113fc6796e1a76204d30c17d9c9f4ed6
Siroshun09 <siro.shun8@gmail.com>
2020-02-15 23:20:19 +0900

    [Update] Javadoc (Spawners 2.0)

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"net.okocraft.spawners"},{"l":"net.okocraft.spawners.events"},{"l":"net.okocraft.spawners.listeners"},{"l":"net.okocraft.spawners.stack"},{"l":"すべてのパッケージ","url":"allpackages-index.html"}]
